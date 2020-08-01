5d947913d8284ca0025fc084e8baedcaaa05cd73
Артём Мальцев <supertomix@yandex.ru>
2020-08-01 13:10:19 +0300

    add classes

---
+++ GameSkillBoxHW/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameSkillBoxHW
+{
+    class Game
+    {
+        public int mode;
+        public static int score = 20;
+        public int SetMode() //Выбор режима игры
+        {
+            Console.WriteLine("Выберите режим игры:\n1)Многопользовательский\n2)Однопользователский");
+            mode = Convert.ToInt32(Console.ReadLine());
+            return mode;
+        }
+        public void StartGame(int mode, Player player1, Player player2) // логика самой игры
+        {
+            while (score > 0)
+
+            {
+                Console.WriteLine($"Ход {player1.name},\nТекущее количество очков:{score}");
+                player1.FirStep(player1);
+                Console.WriteLine($"Ход {player2.name},\nТекущее количество очков:{score}");
+                player2.SecStep(mode, player2);
+            }
+        }
+    }
+}
---
+++ GameSkillBoxHW/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameSkillBoxHW
+{
+    public class Player
+    {
+        public string name;
+        public string setName() //Ввод имени игрока
+        {
+            Console.WriteLine("Введите имя:");
+            name = Console.ReadLine();
+            return name;
+        }
+        public void FirStep(Player player1) // Ход игрока
+        {
+            int curStep = Convert.ToInt32(Console.ReadLine());
+            Game.score -= curStep;
+            if (Game.score == 0) Console.WriteLine($"Победил {player1.name},\n Goodjob!");
+        }
+        public void SecStep(int mode, Player player2) // Ход игрока
+        {
+            if (mode == 1)
+            {
+                int curStep = Convert.ToInt32(Console.ReadLine());
+                Game.score -= curStep;
+                if (Game.score == 0) Console.WriteLine($"Победил {player2.name},\n Goodjob!");
+            }
+            else if (mode == 2)
+            {
+                Random rnd = new Random();
+                Game.score -= rnd.Next(1, 5);
+                if (Game.score == 0) Console.WriteLine($"Победил Робот,\n Goodjob!");
+            }
+        }
+    }
+}
